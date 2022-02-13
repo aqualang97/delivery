a5255251024bcc9172b7509dce825164ca8d768a
Yurii Oboznyi <aualang9797@gmail.com>
2022-02-13 21:31:17 +0200

    Bad trying to make tests

--- internal/repository_innterfaces/user_interfaces.go
+++ internal/repository_innterfaces/user_interfaces.go
@@ -2,6 +2,8 @@ package repository_innterfaces
 
 import "delivery/internal/models"
 
+//go:generate mockgen -source=service.go -destination=mocks/user.go
+
 type UserRepositoryInterface interface {
 	GetUserById(id int) (models.User, error)
 	GetUserByEmail(email string) (models.User, error)
@@ -9,7 +11,6 @@ type UserRepositoryInterface interface {
 	CreateUser(user *models.RegistrationRequest, passwordHash string) (int, error)
 	UpdateUserById(user *models.User) error
 }
-
 type UserAccessTokenRepositoryInterface interface {
 	InsertAccessToken(userToken models.UserAccessToken) error
 	IsExistAccess(userID int) (bool, error)
@@ -30,14 +31,6 @@ type UserRefreshTokenRepositoryInterface interface {
 	DeleteNaturallyExpiredRefreshToken()
 }
 
-type IngredientRepositoryInterface interface {
-	IsExistIngredient(ingredient string) bool
-	InsertIngredient(ingredient string) error
-	DeleteIngredient(ingredient string) error
-	GetIngredientByName(name string) (models.Ingredients, error)
-	GetIngredientIDByName(name string) (int, error)
-	GetIngredientByID(id int) (models.Ingredients, error)
-}
 type OrderProductRepositoryInterface interface {
 	InsertToOrdersProducts(mo models.OrderProducts) (int, error)
 	UpdateNumbersByProductAndOrderID(mo models.OrderProducts) error
@@ -53,44 +46,7 @@ type OrderRepositoryInterface interface {
 	GetOrderByUserIDNotPaidNotCompleted(userID int) (models.Order, error)
 	DeleteOrderByUserID(userID int) error
 }
-type ProductRepositoryInterface interface {
-	GetProductByID(id int) (models.Product, error)
-	GetListOfProdInCategory(catID int) []models.Product
-	GetListOfProdBySupplier(suppID int) []models.Product
-	GetAllProducts() []models.Product
-	InsertToProducts(mp models.Position, productCategoryID int) (int, error)
-	UpdateProductById(mp models.Product) error
-}
-type ProductsCategoriesRepositoryInterface interface {
-	CreateCategory(category models.ProductsCategories) (int, error)
-	DeleteCategory(id int) error
-	GetCategoryByID(id int) (*models.ProductsCategories, error)
-	GetAllCategories() ([]models.ProductsCategories, error)
-}
-type ProductsIngredientsRepositoryInterface interface {
-	InsertProductIngredient(productID, ingredientID int) error
-	GetIngredientsByProductID(id int) ([]models.ProductsIngredients, error)
-	DeleteIngredientByProduct(productID int) error
-}
-type ProductsSuppliersRepositoryInterface interface {
-	IsExistProductSupplier(ps models.ProductsSuppliers) bool
-	InsertProductSupplier(ps models.ProductsSuppliers) error
-	UpdatePriceByExternalData(price float64, extProdID, extSuppID int) error
-	DeleteProductBySupplier(ps models.ProductsSuppliers) error
-	GetAllExternalProductIDByExternalSupplierID(extSuppID int) ([]int, error)
-	IsExist(prodID, suppID int) bool
-}
-type SupplierRepositoryInterface interface {
-	CreateSupplier(supp models.SupplierForParse, categorySupplierID int) (int, error)
-	GetSupplierByID(id int) (models.Supplier, error)
-	GetSupplierByName(name string) ([]models.Supplier, error)
-	GetAllSuppliers() ([]models.Supplier, error)
-	UpdateWorkingHoursByID(ms models.Supplier) error
-}
-type SuppliersCategoriesRepositoryInterface interface {
-	CreateCategory(categories models.SuppliersCategories) error
-	GetSupplierCategoryID(name string) (int, error)
-}
+
 type UserContactRepositoryInterface interface {
 	CreateUserInfo(data models.UserContactData) error
 	GetUserInfoByUserID(userID int) ([]models.UserContactData, error)
